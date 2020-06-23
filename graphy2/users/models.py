2e2ba5a2ed67cbf703168aef4e7c9dfabfdd29bb
James Greenhill <jams@uber.com>
2020-06-22 23:54:58 -0700

    add plane model

--- graphy2/users/models.py
+++ graphy2/users/models.py
@@ -1,7 +1,21 @@
 from django.contrib.auth.models import AbstractUser
-from django.db.models import CharField
+from django.db.models import CharField, Model
 from django.urls import reverse
 from django.utils.translation import ugettext_lazy as _
+
+
+class Plane(Model):
+    name = CharField(_("Name of plane"), blank=True, max_length=255)
+
+    class Meta:
+        verbose_name = _("Plane")
+        verbose_name_plural = _("s")
+
+    def __str__(self):
+        return self.name
+
+    def get_absolute_url(self):
+        return reverse("_detail", kwargs={"pk": self.pk})
 
 
 class User(AbstractUser):
