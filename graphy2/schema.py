b6c7468d7c6ab85339d55ab9f71aec7addf9be44
James Greenhill <fuziontech@gmail.com>
2020-06-26 23:23:27 -0700

    getting the hang of graphene, it's nice

--- graphy2/schema.py
+++ graphy2/schema.py
@@ -3,14 +3,28 @@ import graphene
 
 from .users.models import User as UserModel
 
+
+class Foo(graphene.ObjectType):
+    bar = graphene.String()
+
+    def resolve_bar(self, info):
+        return "foo"
+
+
 class User(DjangoObjectType):
     class Meta:
         model = UserModel
 
+
 class Query(graphene.ObjectType):
     users = graphene.List(User)
+    foos = graphene.List(Foo)
 
     def resolve_users(self, info):
         return UserModel.objects.all()
+
+    def resolve_foos(self, info):
+        return [Foo() for _ in range(0, 5)]
+
 
 schema = graphene.Schema(query=Query)
